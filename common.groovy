142ab542fdef62e6c00d26b6092f09cf1f882133
gsanthoshkrishna <gsanthoshkrishna@gmail.com>
2017-12-25 22:31:34 +0530

    test

--- common.groovy
+++ common.groovy
@@ -1,5 +1,9 @@
 def me = sh (script:'git show --name-only origin/master',returnStdout:true).trim().split('/')
 
+for (i = 0; i <me.length; i++) {
+   println(me[i])
+}
+
 def pipeui = load 'package/ui-web/test2.groovy'
 pipeui.testui()
 
