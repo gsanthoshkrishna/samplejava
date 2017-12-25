16ab60fb382bcc838d2ed6e9c41e10ac42607b6d
gsanthoshkrishna <gsanthoshkrishna@gmail.com>
2017-12-25 22:41:30 +0530

    ttest

--- common.groovy
+++ common.groovy
@@ -2,11 +2,19 @@ def me = sh (script:'git show --name-only origin/master',returnStdout:true).trim
 println("------------output--------------")
 for (i = 0; i <me.length; i++) {
    println(me[i])
+   if(me[i] == 'ui-web')
+   {
+      def pipeui = load 'package/ui-web/test2.groovy'
+      pipeui.testui()
+   }
+   if(me[i] == 'service')
+   {
+      def pipeserv = load 'package/service/test1.groovy'
+      pipeserv.testservice()
+   }
+   
 }
 
-def pipeui = load 'package/ui-web/test2.groovy'
-pipeui.testui()
 
 
-def pipeserv = load 'package/service/test1.groovy'
-pipeserv.testservice()
+
