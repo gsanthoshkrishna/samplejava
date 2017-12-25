ed2c443c3a562650db7e72118b21c03d8fbe09ad
gsanthoshkrishna <gsanthoshkrishna@gmail.com>
2017-12-25 22:29:28 +0530

    rest

--- common.groovy
+++ common.groovy
@@ -1,7 +1,7 @@
 def me = sh (script:'git show --name-only origin/master',returnStdout:true).trim().split('/')
 
 def pipeui = load 'package/ui-web/test2.groovy'
-pipetest.testui()
+pipeui.testui()
 
 
 def pipeserv = load 'package/service/test1.groovy'
